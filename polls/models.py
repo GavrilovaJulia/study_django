2388bdb2ee6a2599afa45ec123175626744c3dea
GavrilovaJulia <july.gavrilova2568@yandex.ru>
2021-06-07 00:50:00 +0300

    add polls, main page

---
+++ polls/models.py
@@ -0,0 +1,31 @@
+import datetime
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200, verbose_name="Текст вопроса")
+    pub_date = models.DateTimeField(default=timezone.now(), verbose_name="Дата публикации")
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def get_absolute_url(self):
+        return f'/polls/{self.id}'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Вопрос")
+    choice_text = models.CharField(max_length=200, verbose_name="Текст ответа")
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+    def get_absolute_url(self):
+        return f'/polls/'
